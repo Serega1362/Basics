a79a33ffcae41d97dca1ab4181cba0192a3d0398
User <1611362>
2022-08-08 15:31:18 +0300

    Initial

---
+++ src/one/Main.java
@@ -0,0 +1,18 @@
+package one;
+/*
+Найдите значение функции: z=((a-3)*b/2)+c;
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введите три целых числа:");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        float z = ((a - 3) * b / 2) + c;
+        System.out.print(z);
+
+    }
+}
